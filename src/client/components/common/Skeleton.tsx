a0550bd5a92e11069bdbdd677aafa6f424e20a69
Erik Veland <erikveland@gmail.com>
2026-03-28 14:01:11 +1000

    feat(design-system): migrate Skeleton to CSS module

--- src/client/components/common/Skeleton.tsx
+++ src/client/components/common/Skeleton.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { cn } from '../../utils/cn';
+import s from './Skeleton.module.css';
 
 interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
 }
 
 export function Skeleton({ className, ...props }: SkeletonProps) {
-  return (
-    <div
-      className={cn('animate-pulse rounded-md bg-[var(--glass-bg-highlight)]', className)}
-      {...props}
-    />
-  );
+  return <div className={cn(s.root, className)} {...props} />;
 }
